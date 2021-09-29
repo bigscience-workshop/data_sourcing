3aabb943e4b0e07c7312e9035d782f1a26c9651b
Yacine Jernite <yacine.jernite@gmail.com>
2021-09-29 07:59:10 -0500

    show_result

--- sourcing_sprint/streamlit_form.py
+++ sourcing_sprint/streamlit_form.py
@@ -60,6 +60,17 @@ indic_languages = [
 ]
 MAX_LANGS = 10
 
+resource_dict = {
+    "type": "",
+    "name": "",
+    "uid": "",
+    "homepage": "",
+    "description": "",
+    "languages": "",
+    "resource_locations": "",
+    "language_locations": "",
+}
+
 ##################
 ## streamlit
 ##################
@@ -102,7 +113,7 @@ You will be asked to fill in information about the dataset object itself as well
 (e.g. Wikipedia, or news sites for most summarization datasets).
 - *Partner organization*:
 """
-resource_type = st.sidebar.radio(
+resource_dict["type"] = st.sidebar.radio(
     label="What resource type are you submitting?",
     options=["Primary source", "Processed dataset", "Partner organization"],
     help=resource_type_help,
@@ -113,19 +124,19 @@ form_col, _, display_col = st.columns([10, 1, 7])
 form_col.markdown("## New catalogue entry: input form\n --- \n")
 
 form_col.markdown("#### Name, ID, Homepage")
-resource_name = form_col.text_input(
+resource_dict["name"] = form_col.text_input(
     label=f"Provide a descriptive name for the resource",
     help="This should be a human-readable name such as e.g. **Le Monde newspaper** (primary source), **EXAMS QA dataset** (processed dataset), or **Creative Commons** (partner organization)",
 )
-resource_uid = form_col.text_input(
+resource_dict["uid"] = form_col.text_input(
     label=f"Provide a short `camel_case` unique identifier for the resource",
     help="For example `le_monde_primary`, `exams_dataset`, or `creative_commons_org`",
 )
-resource_homepage  = form_col.text_input(
+resource_dict["homepage"]  = form_col.text_input(
     label=f"If available, provide a link to the home page for the resource",
     help="e.g. https://www.lemonde.fr/, https://github.com/mhardalov/exams-qa, or https://creativecommons.org/",
 )
-resource_homepage  = form_col.text_area(
+resource_dict["description"] = form_col.text_area(
     label=f"Provide a short description of the resource",
     help="Describe the resource in a few words to a few sentences, the description will be used to index and navigate the catalogue",
 )
@@ -157,25 +168,25 @@ with form_col.expander("Languages"):
                 resource_lang_subgroup = ""
             resource_languages += [(resource_lang_group, resource_lang_subgroup)]
             buttons[lni+1] = st.checkbox(f"Add language {lni+2}")
+    resource_dict["languages"] = resource_languages
 
 with form_col.expander("Locations"):
-    resource_loc = st.text_input(
+    resource_dict["resource_locations"] = st.text_input(
         label="Where is the resource located?",
         help="E.g.: where is the *website* hosted, what is the physical *location of the library*, etc.?",
     )
-    language_loc = st.text_input(
+    resource_dict["language_locations"] = st.text_input(
         label="Where are the language creators located?",
         help="E.g.: where are the *people who write* on the website hosted, where are the *media managed by the library* from, etc.?",
     )
 
-
-if resource_type == "Processed dataset":
+if resource_dict["type"] == "Processed dataset":
     form_col.markdown("#### Processed dataset availability")
     form_col.write("TODO: how to obtain, license, personal information, will you do it")
     form_col.markdown("#### Primary sources of processed dataset")
     form_col.write("TODO: list and either link OR fill out relevant information")
 
-if resource_type == "Primary source":
+if resource_dict["type"] == "Primary source":
     form_col.markdown("#### Primary source type")
     form_col.write("TODO: taxonomy - website/published work/collection")
     form_col.markdown("#### Primary source availability")
@@ -185,4 +196,5 @@ form_col.markdown("#### Media type and format")
 form_col.write("TODO: list of formats and expected processing needs")
 
 display_col.markdown("## New catalogue entry: save output\n --- \n")
-display_col.markdown(f"You are entering a new resource of type: *{resource_type}*")
+display_col.markdown(f"You are entering a new resource of type: *{resource_dict['type']}*")
+display_col.write(resource_dict)
