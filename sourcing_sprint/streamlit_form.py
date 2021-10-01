f1ac27e9e568c5d8bbb91bd4bd9e676aa0c66e73
Yacine Jernite <yacine.jernite@gmail.com>
2021-10-01 18:21:26 -0400

    add map

--- sourcing_sprint/streamlit_form.py
+++ sourcing_sprint/streamlit_form.py
@@ -3,6 +3,9 @@ import json
 import pandas as pd
 import streamlit as st
 
+from folium import Marker
+from folium.plugins import MarkerCluster
+from jinja2 import Template
 from streamlit_folium import folium_static
 
 ##################
@@ -12,6 +15,8 @@ from streamlit_folium import folium_static
 ### Countries and languages
 # from Wikipedia and https://unstats.un.org/unsd/methodology/m49/
 regions, countries, region_tree = json.load(open("unstats_regions_countries.json", encoding="utf-8"))
+country_centers = json.load(open("country_center_coordinates.json", encoding="utf-8"))
+country_mappings = json.load(open("country_mappings.json", encoding="utf-8"))
 
 bcp_47 = json.load(open("bcp47.json", encoding="utf-8"))
 bcp_47_langs = [x for x in bcp_47["subtags"] if x["type"] == "language"]
@@ -41,6 +46,85 @@ resource_dict = {
 
 licenses = json.load(open("licenses.json", encoding="utf-8"))
 MAX_LICENSES = 25
+
+##################
+## Mapping functions
+##################
+WORLD_GEO_URL = "https://raw.githubusercontent.com/python-visualization/folium/master/examples/data/world-countries.json"
+
+ICON_CREATE_FUNCTIOM = '''
+    function(cluster) {
+        var markers = cluster.getAllChildMarkers();
+        var sum = 0;
+        for (var i = 0; i < markers.length; i++) {
+            sum += markers[i].options.props.resources;
+        }
+
+        return L.divIcon({
+             html: '<b>' + sum + '</b>',
+             className: 'marker-cluster marker-cluster-small',
+             iconSize: new L.Point(20, 20)
+        });
+    }
+'''
+
+class MarkerWithProps(Marker):
+    _template = Template(u"""
+        {% macro script(this, kwargs) %}
+        var {{this.get_name()}} = L.marker(
+            [{{this.location[0]}}, {{this.location[1]}}],
+            {
+                icon: new L.Icon.Default(),
+                {%- if this.draggable %}
+                draggable: true,
+                autoPan: true,
+                {%- endif %}
+                {%- if this.props %}
+                props : {{ this.props }}
+                {%- endif %}
+                }
+            )
+            .addTo({{this._parent.get_name()}});
+        {% endmacro %}
+        """)
+    def __init__(self, location, popup=None, tooltip=None, icon=None,
+                 draggable=False, props = None):
+        super(MarkerWithProps, self).__init__(location=location,popup=popup,tooltip=tooltip,icon=icon,draggable=draggable)
+        self.props = json.loads(json.dumps(props))
+
+country_centers = json.load(open("country_center_coordinates.json", encoding="utf-8"))
+country_mappings = json.load(open("country_mappings.json", encoding="utf-8"))
+
+@st.cache(allow_output_mutation=True)
+def make_choro_map(resource_counts, marker_thres=0):
+    world_map = folium.Map(tiles="cartodbpositron", location=[0,0], zoom_start=1.5)
+    marker_cluster = MarkerCluster(icon_create_function=ICON_CREATE_FUNCTIOM)
+    marker_cluster.add_to(world_map)
+    for name, count in resource_counts.items():
+        if count > marker_thres and name in country_centers or name in country_mappings["to_center"]:
+            country_center = country_centers[country_mappings["to_center"].get(name, name)]
+            MarkerWithProps(
+                location = [
+                    country_center["latitude"], country_center["longitude"]],
+                popup = f"Country : {name}<br> \n Resources : {count} <br>",
+                props = {'name': name, 'resources': count},
+            ).add_to(marker_cluster)
+    df_resource_counts = pd.DataFrame(
+        [(country_mappings["to_outline"].get(n, n), c) for n, c in resource_counts.items()],
+        columns=["Name", "Resources"],
+    )
+    folium.Choropleth(
+        geo_data = WORLD_GEO_URL,
+        name="resource map",
+        data=df_resource_counts,
+        columns=["Name", "Resources"],
+        key_on="feature.properties.name",
+        fill_color='PuRd',
+        nan_fill_color='white',
+    ).add_to(world_map)
+    return world_map
+
+
 ##################
 ## streamlit
 ##################
@@ -719,7 +803,30 @@ with viz_col.expander(
     st.write("TODO: add selection widgets")
 
 if viz_mode:
-    viz_col.write("TODO: add interactive map here")
+    # make random count data
+    random_counts = {
+        "France": 45,
+        "United States": 128,
+        "Chile": 10,
+        "Belgium": 4,
+        "Puerto Rico": 3,
+        "Canada": 25,
+        "Greece": 5,
+        "South Africa": 34,
+        "Algeria": 5,
+        "Lybia": 7,
+        "Kenya": 23,
+        "Rwanda": 4,
+        "Vietnam": 10,
+        "Hong Kong": 12,
+        "China": 28,
+        "New Zealand": 12,
+        "Australia": 4,
+        "Iceland": 30,
+    }
+    world_map = make_choro_map(random_counts)
+    with viz_col:
+        folium_static(world_map, width=1200, height=600)
 
 with viz_col.expander(
     "ElasticSearch of resource names and descriptions" if viz_mode else "",
